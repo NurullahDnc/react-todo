155292aeec95e48be9a1f79d2e54b9162073de88
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-12-11 02:00:34 +0300

    form Html olsuturuldu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Form from './componets/Form'
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>MY TODO LİST</h1>
+      </header>
+
+      <Form />
     
     </div>
   );
